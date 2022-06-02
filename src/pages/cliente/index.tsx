80d925beb2682a225e6ca4ff939be96290d41959
Antonia Silva <antonia.josy95@gmail.com>
2022-06-02 16:09:21 -0300

    adicionando cliente

---
+++ src/pages/cliente/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Cliente = () => {
+    return(
+        <>
+            <h1>clientes</h1>
+        </>
+    )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from '../pages/home';
 import { Produto } from '../pages/produtos';
+import { Cliente } from '../pages/cliente';
 
 export const Rotas = () => {
     return (
@@ -8,6 +9,7 @@ export const Rotas = () => {
             <Routes>
                <Route path="/" element={<Home/>}/>
                <Route path="produtos" element={<Produto/>}/>
+               <Route path="cliente" element={<Cliente/>}/>
                <Route path="*" element={<Navigate to="/" replace />} />
              
             </Routes>
